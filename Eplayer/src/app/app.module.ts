00a0625f56bda82de0baf60b6ab4fa29f78d9cc7
Gabriel Campos <gabrielfcampos2017@gmail.com>
2023-01-19 09:15:57 -0300

    banner e game bar

--- Eplayer/src/app/app.module.ts
+++ Eplayer/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
+import { BannerComponent } from './components/template/banner/banner.component';
+import { GameBarComponent } from './components/template/game-bar/game-bar.component';
+import { LojaComponent } from './components/template/loja/loja.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    GameBarComponent,
+    LojaComponent
   ],
   imports: [
     BrowserModule,
---
+++ Eplayer/src/app/components/template/game-bar/game-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-bar',
+  templateUrl: './game-bar.component.html',
+  styleUrls: ['./game-bar.component.css']
+})
+export class GameBarComponent {
+
+}
