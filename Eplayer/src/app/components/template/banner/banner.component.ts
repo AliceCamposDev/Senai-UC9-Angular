2349d550b062494114599e4132ace0ac5d3ec4da
Gabriel Campos <gabrielfcampos2017@gmail.com>
2023-01-27 11:14:58 -0300

    TS banner arrows

--- Eplayer/src/app/components/template/banner/banner.component.ts
+++ Eplayer/src/app/components/template/banner/banner.component.ts
@@ -7,102 +7,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent {
   ngOnInit(): void {
-    let slideAtual = 1
-    let listaSlides = ["url(../../../../assets/img/valorantBanner.png)", "url(../../../../assets/img/csBanner.png)", "url(../../../../assets/img/sonaBanner.png)"]
-    if (document.getElementById("bannerImg") as HTMLDivElement| null) {
-      const bannerImg = document.getElementById("bannerImg");
-      const radio0 = (document.getElementById("radioBanner0") as HTMLInputElement | null);
-      const radio1 = (document.getElementById("radioBanner1") as HTMLInputElement | null);
-      const radio2 = (document.getElementById("radioBanner2") as HTMLInputElement | null);
+    let slideAtual = 2
+    let listaSlides = ["url(../../../../assets/img/sonaBanner.png)", "url(../../../../assets/img/csBanner.png)", "url(../../../../assets/img/valorantBanner.png)"]
+    const bannerImg = (document.getElementById("bannerImg") as HTMLDivElement | null);
+    const radio0 = (document.getElementById("radioBanner0") as HTMLInputElement | null);
+    const radio1 = (document.getElementById("radioBanner1") as HTMLInputElement | null);
+    const radio2 = (document.getElementById("radioBanner2") as HTMLInputElement | null);
+    const bannerLeftArrow = (document.getElementById("bannerLeftArrow") as HTMLInputElement | null);
+    const bannerRightArrow = (document.getElementById("bannerRightArrow") as HTMLInputElement | null);
+    
+    mudarSlideDireita();
+    setInterval(mudarSlideDireita, 5000);
+    /*//TODO: resetar timer se botão clicado*/
 
-      setInterval(mudarSlideDireita, 5000)
-      /*//TODO: resetar timer se botão clicado*/
-      function mudarSlideEsquerda() {
+    bannerLeftArrow?.addEventListener("click",mudarSlideEsquerda);
+    bannerRightArrow?.addEventListener("click",mudarSlideDireita);
 
-        if (slideAtual == 2) {
-          if (bannerImg)
+  
+    function mudarSlideEsquerda() {
+
+      if (slideAtual == 2) {
+        if (bannerImg)
           bannerImg.style.backgroundImage = listaSlides[1];
-          //TODO: AHAHAHAHAHAHAHAHA NAO É NULL CARA ACABEI DE TESTAR ENCIMA AHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAH
-          slideAtual = 1;
-          if (radio0)
-          radio0.checked=false;
-          if (radio1)
-          radio1.checked=true;
-          if (radio2)
-          radio2.checked=false;
+        //TODO: AHAHAHAHAHAHAHAHA NAO É NULL CARA ACABEI DE TESTAR ENCIMA AHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAH
+        slideAtual = 1;
+        if (radio0)
+          radio0.checked = false;
+        if (radio1)
+          radio1.checked = true;
+        if (radio2)
+          radio2.checked = false;
 
-        } else if (slideAtual == 1) {
-          if (bannerImg)
+      } else if (slideAtual == 1) {
+        if (bannerImg)
           bannerImg.style.backgroundImage = listaSlides[0];
 
-          slideAtual = 0;
+        slideAtual = 0;
 
-          if (radio0)
-          radio0.checked=true;
-          if (radio1)
-          radio1.checked=false;
-          if (radio2)
-          radio2.checked=false;
+        if (radio0)
+          radio0.checked = true;
+        if (radio1)
+          radio1.checked = false;
+        if (radio2)
+          radio2.checked = false;
 
-        } else {
-          if (bannerImg)
+      } else {
+        if (bannerImg)
           bannerImg.style.backgroundImage = listaSlides[2];
-          slideAtual = 2;
+        slideAtual = 2;
 
-          if (radio0)
-          radio0.checked=false;
-          if (radio1)
-          radio1.checked=false;
-          if (radio2)
-          radio2.checked=true;
-
-        }
+        if (radio0)
+          radio0.checked = false;
+        if (radio1)
+          radio1.checked = false;
+        if (radio2)
+          radio2.checked = true;
 
       }
 
-      function mudarSlideDireita() {
-
-        if (slideAtual == 2) {
-          if (bannerImg)
-          bannerImg.style.backgroundImage = listaSlides[0];
-          slideAtual = 0;
-          if (radio0)
-          radio0.checked=true;
-          if (radio1)
-          radio1.checked=false;
-          if (radio2)
-          radio2.checked=false;
-
-        } else if (slideAtual == 1) {
-          if (bannerImg)
-          bannerImg.style.backgroundImage = listaSlides[2];
-
-          slideAtual = 2;
-
-          if (radio0)
-          radio0.checked=false;
-          if (radio1)
-          radio1.checked=false;
-          if (radio2)
-          radio2.checked=true;
-
-        } else {
-          if (bannerImg)
-          bannerImg.style.backgroundImage = listaSlides[1];
-          slideAtual = 1;
-
-          if (radio0)
-          radio0.checked=false;
-          if (radio1)
-          radio1.checked=true;
-          if (radio2)
-          radio2.checked=false;
-
-        }
-
-      }
-      console.log("ㅤ██ㅤ  ██\n ██████████ㅤㅤㅤㅤㅤㅤㅤㅤpanㅤㅤ:)\n██ㅤ▌██ㅤ▌██\n██ㅤ ██ㅤ ██\nㅤ████████");
     }
+
+    function mudarSlideDireita() {
+
+      if (slideAtual == 2) {
+        if (bannerImg)
+          bannerImg.style.backgroundImage = listaSlides[0];
+        slideAtual = 0;
+        if (radio0)
+          radio0.checked = true;
+        if (radio1)
+          radio1.checked = false;
+        if (radio2)
+          radio2.checked = false;
+
+      } else if (slideAtual == 1) {
+        if (bannerImg)
+          bannerImg.style.backgroundImage = listaSlides[2];
+
+        slideAtual = 2;
+
+        if (radio0)
+          radio0.checked = false;
+        if (radio1)
+          radio1.checked = false;
+        if (radio2)
+          radio2.checked = true;
+
+      } else {
+        if (bannerImg)
+          bannerImg.style.backgroundImage = listaSlides[1];
+        slideAtual = 1;
+
+        if (radio0)
+          radio0.checked = false;
+        if (radio1)
+          radio1.checked = true;
+        if (radio2)
+          radio2.checked = false;
+
+      }
+
+    }
+    console.log("ㅤ██ㅤ  ██\n ██████████ㅤㅤㅤㅤㅤㅤㅤㅤpanㅤㅤ:)\n██ㅤ▌██ㅤ▌██\n██ㅤ ██ㅤ ██\nㅤ████████");
   }
 }
 
